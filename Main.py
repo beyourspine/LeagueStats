d36a33369175211b916bbbacb2d65991ba9785d3
beyourspine <72161940+beyourspine@users.noreply.github.com>
2023-07-11 01:19:15 +0200

    Major update
    Most features implemented, just need to make it track specific games.

--- Main.py
+++ Main.py
@@ -1,25 +1,35 @@
 import pandas as pd
+import numpy as np
+from os import path
 import riotwatcher
 from riotwatcher import LolWatcher, ApiError
 
 i = 0
-
-lol_watcher = LolWatcher("RGAPI-5fd178df-d0ad-4615-8314-01f864c1b729")
-
+q = 0
+lol_watcher = LolWatcher("RGAPI-5d6748ad-35c4-4c35-a05c-54dfbe074297")
 region = "euw1"
 userName = "FrozenFire2018"
 summoner = lol_watcher.summoner.by_name(region, userName)
 puuid = summoner["puuid"]
 
-matchList = lol_watcher.match.matchlist_by_puuid(region, puuid)
+if path.isfile('gameList.npy') == False:
+    file = open('gameList.npy', 'x')
+    file.close
 
-print(matchList)
+if path.getsize("gameList.npy") == 0:
+    previousMatchList = np.empty(0)
+else:
+    previousMatchList = np.load('gameList.npy')
+    
+matchList = np.array(lol_watcher.match.matchlist_by_puuid(region, puuid, queue = 440))
+matchMask = np.isin(matchList, previousMatchList, invert = True)
+matchList = matchList[matchMask]
 
-querySQL = """
-Select kills, deaths, killingSprees, visionScore, goldEarned, neutralMinionsKilled
-From output
-Group By summonerName
-"""
+if len(matchList) == 0:
+    print("Match List Empty")
+    exit()
+
+np.save( 'gameList' ,np.concatenate((matchList, previousMatchList), axis = 0))
 
 while i < len(matchList):
     try:
@@ -34,21 +44,25 @@ while i < len(matchList):
         print("Error on match " + str(i + 1))
         print(e)
     i+= 1
-
-
-
+    
 output = gameInfo[["kills", "deaths", "killingSprees", "visionScore", "goldEarned", "neutralMinionsKilled", "summonerName", "baitPings", "championName", "enemyMissingPings", "win", "totalMinionsKilled", "role"]].copy()
 output["Creep Score"] = gameInfo["neutralMinionsKilled"] + gameInfo["totalMinionsKilled"]
 outputMask = output["summonerName"] == userName
 output = output[outputMask]
-matchInfo["Index"] = range(20)
+matchInfo["Index"] = range(len(matchInfo))
 matchInfo.set_index("Index", inplace = True)
-output["Index"] = range(20)
+output["Index"] = range(len(output))
 output.set_index("Index", inplace = True)
-output = pd.concat([output, matchInfo[["gameStartTimestamp", "gameMode",]]], axis = 1)
+output = pd.concat([output, matchInfo[["gameStartTimestamp", "gameMode","queueId"]]], axis = 1)
 output["gameStartTimestamp"] = pd.to_datetime(output["gameStartTimestamp"], unit = 'ms')
 output.set_index("gameStartTimestamp", inplace = True)
 
+if path.getsize("output.csv") != 0:
+    print('entered')
+    currentCSV = pd.read_csv("output.csv")
+    currentCSV["gameStartTimestamp"] = currentCSV["gameStartTimestamp"].astype("datetime64[ns]")
+    currentCSV.set_index("gameStartTimestamp", inplace = True)
+    output = pd.concat([output, currentCSV], join = 'inner')
+
+output.sort_index(ascending = False, inplace = True)
 output.to_csv("output.csv")
-
-
